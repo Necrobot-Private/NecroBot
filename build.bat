7feff461fdf616375722192e39de777527430aeb
NzV <donm7v@gmail.com>
2016-09-08 22:07:58 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 echo off
 
-nuget.exe restore "NecroBot for Pokemon Go.sln"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NecroBot for Pokemon Go.sln" /property:Configuration=Release /property:Platform=x86
+nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform=x86
