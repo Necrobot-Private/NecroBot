dee54f538f3deef278b81afa93ba910f37e0497a
SiBangkotan <pramudito@yahoo.com>
2016-09-11 08:14:44 +0700

    Update ILogicSettings.cs

--- PoGo.NecroBot.Logic/Interfaces/Configuration/ILogicSettings.cs
+++ PoGo.NecroBot.Logic/Interfaces/Configuration/ILogicSettings.cs
@@ -133,7 +133,7 @@ namespace PoGo.NecroBot.Logic.Interfaces.Configuration
         bool VerboseRecycling { get; }
         double RecycleInventoryAtUsagePercentage { get; }
         double EvolveKeptPokemonsAtStorageUsagePercentage { get; }
-        int OrWhicheverFirst_EvolveKeptPokemonsAtThisNumber { get; }
+        int EvolveKeptPokemonsOverrideStartIfThisManyReady { get; }
         bool UseSnipeLimit { get; }
         bool UsePokeStopLimit { get; }
         bool UseCatchLimit { get; }
