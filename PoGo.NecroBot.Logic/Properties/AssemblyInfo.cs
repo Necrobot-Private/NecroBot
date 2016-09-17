5c1cd1bfc1fd94301bda93638e2c356a65925a20
MSX752 <msx752@users.noreply.github.com>
2016-09-17 14:49:37 +0300

    updater bug fixed

--- PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.9.9.5")]
+// [assembly: AssemblyVersion1("0.9.9.5")]
 
 
 [assembly: AssemblyVersion("0.9.9.6")]
