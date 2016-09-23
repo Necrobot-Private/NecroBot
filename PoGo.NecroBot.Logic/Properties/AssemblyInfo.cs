401dc6321f5a18a7eb05425c6631e73c578eadbb
NzV <donm7v@gmail.com>
2016-09-23 22:15:00 +0000

    v1.0.0.6

--- PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion1("0.9.9.5")]
 //
 
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
-[assembly: AssemblyInformationalVersion("v1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("v1.0.0.6")]
