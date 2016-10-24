57c935f47ad1fb5f1f10a5ebd67933872361a8ff
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-10-24 12:23:49 +0200

    Optimise Build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 echo off
 
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform=x86
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform="Any CPU"
