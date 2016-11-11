bf62ac21986ba072033cd3db2458f0ba7e72ae2a
NzV <donm7v@gmail.com>
2016-11-11 23:14:59 +0000

    v1.0.0.16

--- PoGo.NecroBot.GUI/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.GUI/Properties/AssemblyInfo.cs
@@ -37,10 +37,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.15")]
+// [assembly: AssemblyVersion("1.0.0.16")]
 
 
-[assembly: AssemblyVersion("1.0.0.15")]
+[assembly: AssemblyVersion("1.0.0.16")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v1.0.0.15")]
+[assembly: AssemblyInformationalVersion("v1.0.0.16")]
 
