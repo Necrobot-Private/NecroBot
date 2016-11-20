7af044f97297e887a3ffd98924792c03538ad83c
Truong Nguyen <samuraitruong@hotmail.com>
2016-11-20 20:18:52 +1100

    some minor validation input

--- PoGo.NecroBot.CLI/BotDataSocketClient.cs
+++ PoGo.NecroBot.CLI/BotDataSocketClient.cs
@@ -237,6 +237,7 @@ namespace PoGo.NecroBot.CLI
 
                 //not your snipe item, return need more encrypt here and configuration to allow catch others item
                 if (string.IsNullOrEmpty(session.LogicSettings.DataSharingIdentifiation) ||
+                    string.IsNullOrEmpty(data.RecieverId) || 
                     data.RecieverId.ToLower() != session.LogicSettings.DataSharingIdentifiation.ToLower()) return;
 
                 var move1 = PokemonMove.Absorb;
