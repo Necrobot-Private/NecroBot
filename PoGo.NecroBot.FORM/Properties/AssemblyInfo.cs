9e19bdb85ddf8ee96a48631ef48f6bf1b78168e1
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-11-22 16:05:00 +0100

    Integrate PoGo.NecroBot.Form

--- PoGo.NecroBot.FORM/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.FORM/Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("PoGo.NecroBot.FORM")]
-[assembly: AssemblyDescription("Modified Version of NecroBot2 and RocketBot")]
+[assembly: AssemblyTitle("NecroBot-Private for Pokémon GO")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PoGo.NecroBot.FORM")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright NecroBot-Private Team © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -40,4 +40,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.0.32")]
-[assembly: AssemblyFileVersion("1.0.0.32")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("v1.0.0.32")]
