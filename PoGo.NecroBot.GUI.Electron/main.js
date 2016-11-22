b7ec28e4b808be79ef07fc3ed8579a23b66c1e70
jjskuld <jjskuld@gmail.com>
2016-11-22 11:10:30 -0800

    Add method to show captcha window

--- PoGo.NecroBot.GUI.Electron/main.js
+++ PoGo.NecroBot.GUI.Electron/main.js
@@ -10,12 +10,32 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let captchaWindow
+
+function showCaptchaWindow(captchaUrl) {
+  captchaWindow = new BrowserWindow({
+    width: 600, 
+	height: 500, 
+	show: false,
+	webPreferences: {
+      nodeIntegration: true,
+      webSecurity: true,
+      preload: path.resolve(path.join(__dirname, 'scripts/preload.js'))
+    }
+  })
+  
+  captchaWindow.loadURL(captchaUrl)
+  captchaWindow.once('ready-to-show', () => {
+	captchaWindow.show()
+  })
+}
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
 	  width: 800, 
-	  height: 600, 
+	  height: 600,
+	  show: false,
 	  webPreferences: {
         nodeIntegration: true,
         webSecurity: true,
@@ -39,6 +59,10 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
+  })
+  
+  mainWindow.once('ready-to-show', () => {
+	mainWindow.show()
   })
 }
 
