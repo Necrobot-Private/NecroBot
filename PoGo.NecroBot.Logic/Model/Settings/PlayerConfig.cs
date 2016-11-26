c01ea61d779b5bf075f80a067f4e5d1ca92d6b8f
jjskuld <jjskuld@gmail.com>
2016-11-26 14:44:50 -0800

    Decrease the default delays which were overly conservative. The evolve animation was also decreased to 20 seconds in official client so match this delay.

--- PoGo.NecroBot.Logic/Model/Settings/PlayerConfig.cs
+++ PoGo.NecroBot.Logic/Model/Settings/PlayerConfig.cs
@@ -7,36 +7,36 @@ namespace PoGo.NecroBot.Logic.Model.Settings
     [JsonObject(Title = "Player Config", Description = "Set your player settings.", ItemRequired = Required.DisallowNull)]
     public class PlayerConfig
     {
-        [DefaultValue(10000)]
+        [DefaultValue(4000)]
         [Range(0, 999999)]
         [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 1)]
-        public int DelayBetweenPlayerActions = 10000;
+        public int DelayBetweenPlayerActions = 4000;
 
         [ExcelConfig(Description = "Set delay time for evolve action", Position = 2)]
-        [DefaultValue(30000)]
-        [Range(0, 999999)]
-        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 2)]
-        public int EvolveActionDelay = 30000;
-
-        [ExcelConfig(Description = "Set delay time for transfer action", Position = 3)]
         [DefaultValue(20000)]
         [Range(0, 999999)]
-        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 3)]
-        public int TransferActionDelay = 20000;
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 2)]
+        public int EvolveActionDelay = 20000;
 
-        [ExcelConfig(Description = "Set delay time for recycle item ", Position = 4)]
-        [DefaultValue(25000)]
+        [ExcelConfig(Description = "Set delay time for transfer action", Position = 3)]
+        [DefaultValue(5000)]
+        [Range(0, 999999)]
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 3)]
+        public int TransferActionDelay = 5000;
+
+        [ExcelConfig(Description = "Set delay time for recycle item", Position = 4)]
+        [DefaultValue(1000)]
         [Range(0, 999999)]
         [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 4)]
-        public int RecycleActionDelay = 25000;
+        public int RecycleActionDelay = 1000;
 
         [ExcelConfig(Description = "Set delay time for rename pokemon action", Position = 5)]
-        [DefaultValue(60000)]
+        [DefaultValue(2000)]
         [Range(0, 999999)]
         [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 5)]
-        public int RenamePokemonActionDelay = 60000;
+        public int RenamePokemonActionDelay = 2000;
 
-        [ExcelConfig(Description = "Set delay time for random  action", Position = 6)]
+        [ExcelConfig(Description = "Set delay time for random action", Position = 6)]
         [DefaultValue(true)]
         [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 6)]
         public bool UseNearActionRandom = true;
