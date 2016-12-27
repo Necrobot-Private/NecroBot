31e19e8b11bb1547477a69af12b090a2ca341d19
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-12-27 07:11:05 +0100

    Update forms

--- NecroBot2/Properties/AssemblyInfo.cs
+++ NecroBot2/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NecroBot-Private Team")]
-[assembly: AssemblyProduct("RocketBot")]
+[assembly: AssemblyProduct("Necrobot2")]
 [assembly: AssemblyCopyright("Copyright NecroBot-Private Team © 2016")]
 [assembly: AssemblyTrademark("NecroBot-Private Team")]
 [assembly: AssemblyCulture("")]
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.32")]
+[assembly: AssemblyVersion("1.0.0.35")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v1.0.0.32")]
+[assembly: AssemblyInformationalVersion("v1.0.0.35")]
