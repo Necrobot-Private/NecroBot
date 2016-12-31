d40e4bb9416606c132ac049a50cbecad8bd50372
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-12-31 20:46:36 +0100

    v1.0.0.45

--- NecroBot2/Properties/AssemblyInfo.cs
+++ NecroBot2/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.42")]
+[assembly: AssemblyVersion("1.0.0.45")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v1.0.0.42")]
+[assembly: AssemblyInformationalVersion("v1.0.0.45")]
