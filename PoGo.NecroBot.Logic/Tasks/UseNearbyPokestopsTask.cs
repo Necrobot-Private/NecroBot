bdc3f788367d962723d04d1f6fea895b45e4df38
ziomber <ziomber@yahoo.com>
2017-01-15 00:16:11 +0100

    Do not go to fort where is deployed your pokemon. A part of [Optimization] Skip walking to gyms that are full and/or owned by another team #512

--- PoGo.NecroBot.Logic/Tasks/UseNearbyPokestopsTask.cs
+++ PoGo.NecroBot.Logic/Tasks/UseNearbyPokestopsTask.cs
@@ -185,7 +185,12 @@ namespace PoGo.NecroBot.Logic.Tasks
                 //TODO : A logic need to be add for handle this  case?
             };
 
-            var forts = session.Forts.Where(p => p.CooldownCompleteTimestampMs < DateTime.UtcNow.ToUnixTime()).ToList();
+            var deployedPokemons = await session.Inventory.GetDeployedPokemons();
+
+            var forts = session.Forts
+                .Where(p => p.CooldownCompleteTimestampMs < DateTime.UtcNow.ToUnixTime())
+                .Where(f => deployedPokemons == null || (f.Type == FortType.Gym && !deployedPokemons.Any(a=>a.DeployedFortId == f.Id) || f.Type == FortType.Checkpoint)) // don't go to fort where is yours pokemon already deployed
+                .ToList();
             forts = forts.OrderBy(
                         p =>
                             session.Navigation.WalkStrategy.CalculateDistance(
