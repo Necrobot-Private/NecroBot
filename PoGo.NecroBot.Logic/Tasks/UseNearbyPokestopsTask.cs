30d3f38890180588f3e065688587e9f015e73824
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-01-16 18:35:55 +0100

    .

--- PoGo.NecroBot.Logic/Tasks/UseNearbyPokestopsTask.cs
+++ PoGo.NecroBot.Logic/Tasks/UseNearbyPokestopsTask.cs
@@ -185,14 +185,7 @@ namespace PoGo.NecroBot.Logic.Tasks
                 //TODO : A logic need to be add for handle this  case?
             };
 
-            var deployedPokemons = await session.Inventory.GetDeployedPokemons();
-
-            var forts = session.Forts
-                .Where(p => p.CooldownCompleteTimestampMs < DateTime.UtcNow.ToUnixTime())
-                .Where(f => deployedPokemons == null || !(f.Type == FortType.Gym && deployedPokemons.Any(a => a.DeployedFortId == f.Id))) // don't go to fort where is yours pokemon already deployed
-                .Where(l => !(l.OwnedByTeam != session.Profile.PlayerData.Team && UseGymBattleTask.GetGymLevel(l.GymPoints) > session.LogicSettings.GymConfig.MaxGymLevelToAttack)) // don't go to other's gym where lvl is too high 
-                .ToList();
-
+            var forts = session.Forts.Where(p => p.CooldownCompleteTimestampMs < DateTime.UtcNow.ToUnixTime()).ToList();
             forts = forts.OrderBy(
                         p =>
                             session.Navigation.WalkStrategy.CalculateDistance(
