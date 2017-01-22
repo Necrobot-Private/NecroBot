f3c6c559698b7530cd4201a72654da03a1ac26e4
jjskuld <jjskuld@gmail.com>
2017-01-22 08:33:16 +0000

    v1.0.0.69

--- PoGo.NecroBot.CLI/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.CLI/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion1("0.9.9.5")]
 
 
-[assembly: AssemblyVersion("1.0.0.68")]
+[assembly: AssemblyVersion("1.0.0.69")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v1.0.0.68")]
+[assembly: AssemblyInformationalVersion("v1.0.0.69")]
 
