57466a5d534fb291f13818dc6cd5a0b1ed5b4fe4
Skuld <jjskuld@gmail.com>
2017-01-30 11:10:53 -0800

    Clean up transfer and evolve logic so that WPF pokemon inventory is updated properly.

--- PoGo.NecroBot.Logic/Inventory.cs
+++ PoGo.NecroBot.Logic/Inventory.cs
@@ -20,6 +20,7 @@ using POGOProtos.Inventory;
 using POGOProtos.Inventory.Item;
 using POGOProtos.Networking.Responses;
 using POGOProtos.Settings.Master;
+using PokemonGo.RocketAPI.Helpers;
 
 #endregion
 
@@ -556,11 +557,51 @@ namespace PoGo.NecroBot.Logic
             return _pokemonSettings;
         }
 
+        public async Task<bool> CanEvolvePokemon(PokemonData pokemon, IEnumerable<PokemonData> pokemonsToEvolve = null)
+        {
+            // Can't evolve pokemon in gyms.
+            if (!string.IsNullOrEmpty(pokemon.DeployedFortId))
+                return false;
+
+            IEnumerable<PokemonSettings> pokemonSettings = await GetPokemonSettings();
+            var settings = pokemonSettings.SingleOrDefault(x => x.PokemonId == pokemon.PokemonId);
+            
+            // Can't evolve pokemon that are not evolvable.
+            if (settings.EvolutionIds.Count == 0)
+                return false;
+
+            List<Candy> pokemonFamilies = await GetPokemonFamilies();
+            int familyCandy = PokemonInfo.GetCandy(pokemon, pokemonFamilies, pokemonSettings);
+            
+            //DO NOT CHANGE! TESTED AND WORKS
+            //TRUONG: temporary change 1 to 2 to fix not enought resource when evolve. not a big deal when we keep few candy.
+            int pokemonCandyNeededAlready;
+            if (pokemonsToEvolve != null)
+            {
+                // Candy needed to evolve multiple pokemon.
+                pokemonCandyNeededAlready =
+                    (pokemonsToEvolve.Count(
+                        p => pokemonSettings.FirstOrDefault(x => x.PokemonId == p.PokemonId) != null &&
+                        pokemonSettings.FirstOrDefault(x => x.PokemonId == p.PokemonId).FamilyId == settings.FamilyId) + 2) *
+                    settings.CandyToEvolve;
+            }
+            else
+            {
+                // Candy needed to evolve a single pokemon.
+                pokemonCandyNeededAlready = settings.CandyToEvolve;
+            }
+
+            // Can't evolve if not enough candy.
+            if (familyCandy < pokemonCandyNeededAlready)
+                return false;
+
+            return true;
+        }
+
         public async Task<IEnumerable<PokemonData>> GetPokemonToEvolve(IEnumerable<PokemonId> filter = null)
         {
-            var myPokemon = GetPokemons();
-            myPokemon = myPokemon.Where(p => p.DeployedFortId == string.Empty).OrderByDescending(p => p.Cp);
-            //Don't evolve pokemon in gyms
+            IEnumerable<PokemonData> myPokemon = GetPokemons().OrderByDescending(p => p.Cp);
+            
             IEnumerable<PokemonId> pokemonIds = filter as PokemonId[] ?? filter.ToArray();
             if (pokemonIds.Any())
             {
@@ -576,34 +617,13 @@ namespace PoGo.NecroBot.Logic
                     myPokemon.Where(
                         p => PokemonInfo.CalculatePokemonPerfection(p) >= _logicSettings.EvolveAboveIvValue);
             }
+
             var pokemons = myPokemon.ToList();
-
-            var myPokemonSettings = await GetPokemonSettings();
-            var pokemonSettings = myPokemonSettings.ToList();
-
-            var myPokemonFamilies = await GetPokemonFamilies();
-            var pokemonFamilies = myPokemonFamilies.ToArray();
-
+            
             var pokemonToEvolve = new List<PokemonData>();
             foreach (var pokemon in pokemons)
             {
-                var settings = pokemonSettings.SingleOrDefault(x => x.PokemonId == pokemon.PokemonId);
-                
-                var familyCandy = pokemonFamilies.SingleOrDefault(x => settings.FamilyId == x.FamilyId);
-
-                //Don't evolve if we can't evolve it
-                if (settings.EvolutionIds.Count == 0)
-                    continue;
-                //DO NOT CHANGE! TESTED AND WORKS
-                //TRUONG: temporary change 1 to 2 to fix not enought resource when evolve. not a big deal when we keep few candy 
-
-                var pokemonCandyNeededAlready =
-                    (pokemonToEvolve.Count(
-                        p => pokemonSettings.FirstOrDefault(x => x.PokemonId == p.PokemonId) != null && 
-                        pokemonSettings.FirstOrDefault(x => x.PokemonId == p.PokemonId).FamilyId == settings.FamilyId) + 2) *
-                    settings.CandyToEvolve;
-
-                if (familyCandy.Candy_ >= pokemonCandyNeededAlready)
+                if (await CanEvolvePokemon(pokemon, pokemonToEvolve))
                 {
                     pokemonToEvolve.Add(pokemon);
                 }
@@ -628,6 +648,34 @@ namespace PoGo.NecroBot.Logic
             return new LevelUpRewardsResponse();
         }
 
+        public bool CanUpgradePokemon(PokemonData pokemon)
+        {
+            // Can't upgrade pokemon in gyms.
+            if (!string.IsNullOrEmpty(pokemon.DeployedFortId))
+                return false;
+
+            var playerLevel = GetPlayerStats().FirstOrDefault().Level;
+            var pokemonLevel = PokemonInfo.GetLevel(pokemon);
+
+            // Can't evolve unless pokemon level is lower than trainer.
+            if (pokemonLevel >= playerLevel)
+                return false;
+            
+            int familyCandy = GetCandy(pokemon.PokemonId);
+
+            // Can't evolve if not enough candy.
+            int pokemonCandyNeededAlready = PokemonCpUtils.GetCandyCostsForPowerup(pokemon.CpMultiplier + pokemon.AdditionalCpMultiplier);
+            if (familyCandy < pokemonCandyNeededAlready)
+                return false;
+
+            // Can't evolve if not enough stardust.
+            var stardustToUpgrade = PokemonCpUtils.GetStardustCostsForPowerup(pokemon.CpMultiplier + pokemon.AdditionalCpMultiplier);
+            if (GetStarDust() < stardustToUpgrade)
+                return false;
+
+            return true;
+        }
+
         public List<PokemonData> GetPokemonToUpgrade()
         {
             var upgradePokemon = new List<PokemonData>();
@@ -635,8 +683,8 @@ namespace PoGo.NecroBot.Logic
             if (!_logicSettings.AutomaticallyLevelUpPokemon)
                 return upgradePokemon;
 
-            var myPokemon = GetPokemons();
-            myPokemon = myPokemon.Where(p => p.DeployedFortId == string.Empty);
+            var myPokemon = GetPokemons().Where(p => CanUpgradePokemon(p));
+
             var grouped = myPokemon.GroupBy(p => p.PokemonId);
 
             Parallel.ForEach(grouped, (group) =>
--- PoGo.Necrobot.Window/Model/PokemonListModel.cs
+++ PoGo.Necrobot.Window/Model/PokemonListModel.cs
@@ -1,51 +1,45 @@
 ﻿using POGOProtos.Data;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using POGOProtos.Enums;
 using PoGo.NecroBot.Logic.Event;
 using POGOProtos.Inventory;
 using POGOProtos.Settings.Master;
 using PoGo.NecroBot.Logic.Event.Inventory;
+using PoGo.NecroBot.Logic.State;
 
 namespace PoGo.Necrobot.Window.Model
 {
     public class PokemonListModel : ViewModelBase
     {
+        public PokemonListModel(ISession session)
+        {
+            this.Session = Session;
+        }
+
         public ObservableCollection<PokemonDataViewModel> Pokemons { get; set; }
 
         internal void Update(List<PokemonData> pokemons, List<Candy> candies, List<PokemonSettings> pokemonSettings)
         {
-                //var families = Session.Inventory.GetPokemonFamilies().Result;
-                //var settings = Session.Inventory.GetPokemonSettings().Result;
+            foreach (var item in pokemons)
+            {
+                var existing = Pokemons.FirstOrDefault(x => x.Id == item.Id);
 
-                foreach (var item in pokemons)
+                if (existing != null)
                 {
-
-                    var existing = Pokemons.FirstOrDefault(x => x.Id == item.Id);
-
-                    if (existing != null)
-                    {
-                        existing.UpdateWith(item);
-                        continue;
-                    }
-                    var setting = pokemonSettings.FirstOrDefault(x => x.PokemonId == item.PokemonId);
-
-                    var family = candies.FirstOrDefault(x => x.FamilyId == setting.FamilyId);
-
-                    Pokemons.Add(new PokemonDataViewModel(item, setting, family));
+                    existing.UpdateWith(item);
                 }
-          //  });
+                else
+                {
+                    Pokemons.Add(new PokemonDataViewModel(this.Session, item));
+                }
+            }
         }
 
         internal void OnUpgradeEnd(FinishUpgradeEvent e)
         {
             var id = e.PokemonId;
             var model = Get(id);
-            model.AllowPowerup = e.AllowUpgrade;
             model.PowerupText = "Upgrade";
         }
 
@@ -54,50 +48,42 @@ namespace PoGo.Necrobot.Window.Model
             var id = e.Pokemon.Id;
             var model = Get(id);
             model.PokemonData = e.Pokemon;
-            model.CP = e.Cp;
-            model.Candy = e.FamilyCandies;
-            model.HP = e.Pokemon.Stamina;
-
         }
+
         public void OnFavorited(FavoriteEvent ev)
         {
             var result = ev.FavoritePokemonResponse.Result == POGOProtos.Networking.Responses.SetFavoritePokemonResponse.Types.Result.Success;
             var id = ev.Pokemon.Id;
             var model = Get(id);
             model.IsFavoriting = false;
-            if (!result)
-            {
-                model.Favorited = !model.Favorited;
-            }
+            model.PokemonData = ev.Pokemon;
         }
 
         private PokemonDataViewModel Get(ulong id)
         {
             return this.Pokemons.FirstOrDefault(x => x.Id == id);
         }
+
         internal void OnEvolved(PokemonEvolveEvent ev)
         {
-            var exist = Get(ev.OriginalId);
-            if(exist != null && ev.Result == POGOProtos.Networking.Responses.EvolvePokemonResponse.Types.Result.Success)
+            if (ev.Result == POGOProtos.Networking.Responses.EvolvePokemonResponse.Types.Result.Success)
             {
-                this.Pokemons.Remove(exist);
-                var newItem = new PokemonDataViewModel(ev.EvolvedPokemon, ev.PokemonSetting, ev.Family);
+                var exist = Get(ev.OriginalId);
+                if (exist != null)
+                    this.Pokemons.Remove(exist);
+
+                var newItem = new PokemonDataViewModel(this.Session, ev.EvolvedPokemon);
                 this.Pokemons.Add(newItem);
 
-                foreach (var item in this.Pokemons)
+                foreach (var item in this.Pokemons.Where(p => p.FamilyId == newItem.FamilyId))
                 {
-                    if (item.PokemonSettings != null && item.PokemonSettings.FamilyId == ev.Family?.FamilyId)
-                    {
-                        item.Candy = ev.Family.Candy_;
-                    }
+                    item.RaisePropertyChanged("Candy");
                 }
-
-            } 
+            }
         }
 
         public void Transfer(List<ulong> pokemonIds)
         {
-
             foreach (var item in pokemonIds)
             {
                 Transfer(item);
@@ -125,14 +111,12 @@ namespace PoGo.Necrobot.Window.Model
         internal void OnTransfer(TransferPokemonEvent e)
         {
             this.Remove(e.Id);
-            foreach (var item in this.Pokemons)
+            foreach (var item in this.Pokemons.Where(p => p.FamilyId == e.FamilyId))
             {
-                if(item.PokemonSettings != null && item.PokemonSettings.FamilyId == e.FamilyId)
-                {
-                    item.Candy = e.FamilyCandies;
-                }
+                item.RaisePropertyChanged("Candy");
             }
         }
+
         internal bool Favorite(ulong pokemonId)
         {
             var pkm = Get(pokemonId);
@@ -141,10 +125,10 @@ namespace PoGo.Necrobot.Window.Model
             {
                 pkm.IsFavoriting = true;
             }
-            pkm.Favorited = !pkm.Favorited;
 
             return pkm.Favorited;
         }
+
         internal void Evolve(ulong pokemonId)
         {
             var pkm = Pokemons.FirstOrDefault(x => x.Id == pokemonId);
@@ -162,7 +146,6 @@ namespace PoGo.Necrobot.Window.Model
 
             if (pkm != null)
             {
-                pkm.AllowPowerup = false;
                 pkm.PowerupText = "Upgrading...";
                 pkm.RaisePropertyChanged("PowerupText");
                 pkm.RaisePropertyChanged("AllowPowerup");
