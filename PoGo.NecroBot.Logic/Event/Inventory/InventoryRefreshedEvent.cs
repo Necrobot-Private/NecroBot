921e8b950aedc055ff8176e364ee22786c23d636
Skuld <jjskuld@gmail.com>
2017-01-31 17:06:48 -0800

    Bug fix - Fix occasional crashes when transferring pokemon using GUI.

--- PoGo.NecroBot.Logic/Event/Inventory/InventoryRefreshedEvent.cs
+++ PoGo.NecroBot.Logic/Event/Inventory/InventoryRefreshedEvent.cs
@@ -9,24 +9,20 @@ namespace PoGo.NecroBot.Logic.Event.Inventory
     public class InventoryRefreshedEvent : IEvent
     {
         public IEnumerable<PlayerStats> PlayerStats { get; set; }
-
-        public GetInventoryResponse Inventory { get; set; }
-
+        
         public List<PokemonSettings> PokemonSettings { get; set; }
 
         public List<Candy> Candies { get; set; }
 
-        public InventoryRefreshedEvent(GetInventoryResponse e, List<PokemonSettings> settings, List<Candy> candy)
+        public InventoryRefreshedEvent(List<PokemonSettings> settings, List<Candy> candy)
         {
             this.Candies = candy;
             this.PokemonSettings = settings;
-            this.Inventory = e;
         }
 
-        public InventoryRefreshedEvent(GetInventoryResponse args, IEnumerable<PlayerStats> playerStats,
+        public InventoryRefreshedEvent(IEnumerable<PlayerStats> playerStats,
             List<PokemonSettings> pokemonSettings, List<Candy> candy)
         {
-            this.Inventory = args;
             this.PlayerStats = playerStats;
             this.PokemonSettings = pokemonSettings;
             this.Candies = candy;
