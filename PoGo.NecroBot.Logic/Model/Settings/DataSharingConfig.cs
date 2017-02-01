1112c32f6ed45439b2769d5098cf52f67ea9a79e
samuraitruong <samuraitruong@hotmail.com>
2017-02-01 20:41:13 +1100

    move win to top of antifact list

--- PoGo.NecroBot.Logic/Model/Settings/DataSharingConfig.cs
+++ PoGo.NecroBot.Logic/Model/Settings/DataSharingConfig.cs
@@ -40,7 +40,7 @@ namespace PoGo.NecroBot.Logic.Model.Settings
         public string DataServiceIdentification { get; set; }
 
         [ExcelConfig(Description = "The authozied access key to use snipe data", Position = 4)]
-        [DefaultValue(false)]
+        [DefaultValue("")]
         [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 1)]
         public string SnipeDataAccessKey { get;  set; }
     }
