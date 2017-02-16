5e2ff98b132ff62bbc9bd05bccb5f5fdcc63938c
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-02-16 01:18:56 +0100

    review this

---
+++ PoGo.NecroBot.Logic/Event/LootPokestopEvent.cs
@@ -0,0 +1,10 @@
+﻿using PoGo.NecroBot.Logic.Event;
+using POGOProtos.Map.Fort;
+
+namespace PoGo.NecroBot.Logic.Event
+{
+    public class LootPokestopEvent : IEvent
+    {
+        public FortData Pokestop;
+    }
+}
