184e652721f38188ae865b8816cf6e2a7018ab01
Skuld <jjskuld@gmail.com>
2017-02-23 18:26:33 -0800

    Bug fix - When encountering nearby pokemon, always update location first to avoid out of range encounter errors.

--- PoGo.NecroBot.Logic/Tasks/CatchNearbyPokemonsTask.cs
+++ PoGo.NecroBot.Logic/Tasks/CatchNearbyPokemonsTask.cs
@@ -15,6 +15,7 @@ using POGOProtos.Inventory.Item;
 using POGOProtos.Map.Pokemon;
 using POGOProtos.Networking.Responses;
 using TinyIoC;
+using GeoCoordinatePortable;
 
 #endregion
 
@@ -60,6 +61,8 @@ namespace PoGo.NecroBot.Logic.Tasks
             if (priorityPokemon != null)
             {
                 pokemons.Insert(0, priorityPokemon);
+                LocationUtils.UpdatePlayerLocationWithAltitude(session,
+                        new GeoCoordinate(priorityPokemon.Latitude, priorityPokemon.Longitude, session.Client.CurrentAltitude), 0); // Set speed to 0 for random speed.
                 encounter = await session.Client.Encounter
                     .EncounterPokemon(priorityPokemon.EncounterId, priorityPokemon.SpawnPointId);
 
@@ -122,6 +125,8 @@ namespace PoGo.NecroBot.Logic.Tasks
 
                 if (encounter == null || encounter.Status != EncounterResponse.Types.Status.EncounterSuccess)
                 {
+                    LocationUtils.UpdatePlayerLocationWithAltitude(session,
+                        new GeoCoordinate(pokemon.Latitude, pokemon.Longitude, session.Client.CurrentAltitude), 0); // Set speed to 0 for random speed.
                     encounter =
                         await session.Client.Encounter.EncounterPokemon(pokemon.EncounterId, pokemon.SpawnPointId);
                 }
