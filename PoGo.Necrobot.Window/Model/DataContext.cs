4b9edfab075308baf73f3ac83e69cad2ed207ff9
samuraitruong <samuraitruong@hotmail.com>
2017-03-02 15:53:43 +1100

    misc changes

--- PoGo.Necrobot.Window/Model/DataContext.cs
+++ PoGo.Necrobot.Window/Model/DataContext.cs
@@ -44,7 +44,7 @@ namespace PoGo.Necrobot.Window.Model
             UI = new UIViewModel();
             Map = new MapViewModel();
 
-            MaxItemStorage = 350;
+            MaxItemStorage = 380;
             ItemsList = new ItemsListViewModel();
             Sidebar = new SidebarViewModel();
             internalPokemons = new List<PokemonData>();
