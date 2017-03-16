961ac5537bacbfb1cdaa1b4f9e247756ab5970e4
Executive Dronpes <lordhajisinbox@gmail.com>
2017-03-16 12:40:51 +0530

    Using serebii instead rankedboost

--- PoGo.Necrobot.Window/Converters/PokemonIdToImageConverter.cs
+++ PoGo.Necrobot.Window/Converters/PokemonIdToImageConverter.cs
@@ -16,13 +16,8 @@ namespace PoGo.Necrobot.Window.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var pokemonId = (PokemonId)value;
-            if ((int)pokemonId > 151)
-            {
-                //http://www.serebii.net/pokemongo/pokemon/145.png maybe better
-                return $"https://rankedboost.com/wp-content/plugins/ice/riot/poksimages/pokemons2/{(int)pokemonId:000}.png";
-            }
 
-            return $"https://rankedboost.com/wp-content/plugins/ice/riot/poksimages/pokemons/{(int)pokemonId:000}.png";
+            return $"http://www.serebii.net/pokemongo/pokemon/{(int)pokemonId:000}.png";
 
         }
 
