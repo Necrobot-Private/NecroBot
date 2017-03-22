795bdf00a88998405e46c238c17e8c0462281d22
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-03-22 20:34:59 +0100

    .

--- RocketBot2/Forms/MainForm.cs
+++ RocketBot2/Forms/MainForm.cs
@@ -536,9 +536,11 @@ namespace RocketBot2.Forms
                     var _item = new ToolStripMenuItem();
                     _item.Text = _bot.Username;
                     _item.Click += delegate
-                    {                       
-                        if (!_botStarted) _session.ReInitSessionWithNextBot(_bot);
+                    {
+                        if (!_botStarted)
+                            _session.ReInitSessionWithNextBot(_bot);
                         accountManager.SwitchAccountTo(_bot);
+                        Instance.checkBoxAutoRefresh.CheckState = CheckState.Indeterminate;
                     };
 
                     if (_item.Text == bot.Username)
@@ -554,7 +556,7 @@ namespace RocketBot2.Forms
                 _session.ReInitSessionWithNextBot(bot);
                 menuStrip1.Items.Remove(accountsToolStripMenuItem);
             }
-
+            
             _machine = machine;
             _settings = settings;
             _excelConfigAllow = excelConfigAllow;
@@ -629,6 +631,10 @@ namespace RocketBot2.Forms
 
         private void InitializePokestopsAndRoute()
         {
+            //get optimized route
+            var pokeStops = RouteOptimizeUtil.Optimize(_session.Forts.ToArray(), _session.Client.CurrentLatitude,
+                    _session.Client.CurrentLongitude);
+
             SynchronizationContext.Post(o =>
             {
                 _playerOverlay.Markers.Clear();
@@ -636,10 +642,6 @@ namespace RocketBot2.Forms
                 _pokestopsOverlay.Markers.Clear();
                 _pokestopsOverlay.Routes.Clear();
                 _playerLocations.Clear();
-
-                //get optimized route
-                var pokeStops = RouteOptimizeUtil.Optimize(_session.Forts.ToArray(), _session.Client.CurrentLatitude,
-                    _session.Client.CurrentLongitude);
 
                 _routePoints =
                     (from pokeStop in pokeStops
@@ -904,6 +906,7 @@ namespace RocketBot2.Forms
             Instance.Text = text;
             Instance.statusLabel.Text = text;
             Console.Title = text;
+
 
             if (checkBoxAutoRefresh.Checked)
                 await ReloadPokemonList().ConfigureAwait(false);
@@ -1170,14 +1173,16 @@ namespace RocketBot2.Forms
                 {
                     case DialogResult.Yes:
                         await Task.Run(async () => { await UpgradeSinglePokemonTask.Execute(_session, pokemon.Id, true /* upgrade x times */); });
+                        if (!checkBoxAutoRefresh.Checked)
+                            await ReloadPokemonList().ConfigureAwait(false);
                         break;
                     case DialogResult.No:
                         await Task.Run(async () => { await UpgradeSinglePokemonTask.Execute(_session, pokemon.Id, false, 1 /* Only upgrade 1 time */); });
+                        if (!checkBoxAutoRefresh.Checked)
+                            await ReloadPokemonList().ConfigureAwait(false);
                         break;
                 }
             }
-            if (!checkBoxAutoRefresh.Checked)
-                await ReloadPokemonList().ConfigureAwait(false);
         }
 
         private void EvolvePokemon(PokemonData pokemon)
@@ -1196,6 +1201,8 @@ namespace RocketBot2.Forms
                             await ReloadPokemonList().ConfigureAwait(false);
                         form.Close();
                     };
+                    /*item.MouseLeave += delegate { item.BackColor = Color.Transparent; };
+                    item.MouseEnter += delegate { item.BackColor = Color.LightGreen; };*/
                     form.flpPokemonToEvole.Controls.Add(item);
                 }
                 form.ShowDialog();
@@ -1244,7 +1251,7 @@ namespace RocketBot2.Forms
                         .Select(x => x.PokemonSettings)
                         .ToList();
 
-                PokemonObject.Initilize(_session, templates);
+                PokemonObject.Initilize(templates);
 
                 var pokemons = 
                    _session.Inventory.GetPokemons().Result
@@ -1257,8 +1264,7 @@ namespace RocketBot2.Forms
 
                 foreach (var pokemon in pokemons)
                 {
-                    var pokemonObject = new PokemonObject(pokemon);
-                    //pokemonObject.Candy = PokemonInfo.GetCandy(_session, pokemon);
+                    var pokemonObject = new PokemonObject(_session, pokemon);
                     pokemonObjects.Add(pokemonObject);
                 }
 
