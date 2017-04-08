a1929819698208bd1174781c75e8c41cddb9e04f
CDAGaming <videodude12>
2017-04-08 01:15:20 -0500

    Some More Optimizations
    Affected Files:
    GoogleElevationService
    GrayScaleConverter
    ItemIdToImageConverter
    ListPokemonIdConverter

--- PoGo.Necrobot.Window/Converters/ItemIdToImageConverter.cs
+++ PoGo.Necrobot.Window/Converters/ItemIdToImageConverter.cs
@@ -12,31 +12,31 @@ namespace PoGo.Necrobot.Window.Converters
 
         Dictionary<ItemId, string> resources = new Dictionary<ItemId, string>()
         {
-            { ItemId.ItemIncubatorBasic,"https://raw.githubusercontent.com/Superviral/Pokemon-GO-App-Assets-and-Images/master/App%20Converted%20Images/EggIncubator.png" },
-            {ItemId.ItemIncubatorBasicUnlimited,"http://i.imgur.com/ZBRdMuy.png" },
-            {ItemId.ItemPokeBall, "https://raw.githubusercontent.com/Superviral/Pokemon-GO-App-Assets-and-Images/master/App%20Converted%20Images/pokeball_store.png" },
-            {ItemId.ItemGreatBall, "http://i.imgur.com/ZP3DlMs.png" },
-            {ItemId.ItemUltraBall, "http://i.imgur.com/nGsWvcR.png" },
-            {ItemId.ItemMasterBall,"http://i.imgur.com/cyLuJEq.png" } ,
-            {ItemId.ItemPotion,"http://i.imgur.com/6MQCO4g.png" } ,
-            {ItemId.ItemSuperPotion,"http://i.imgur.com/LFaT9Lw.png" } ,
-            {ItemId.ItemHyperPotion,"http://i.imgur.com/ay1jWQF.png" },
-            {ItemId.ItemMaxPotion,"http://i.imgur.com/qEqi8Su.png" } ,
-            {ItemId.ItemRevive,"http://i.imgur.com/tswFn3H.png" } ,
-            {ItemId.ItemMaxRevive,"http://i.imgur.com/qa2Rqnw.png" } ,
-            {ItemId.ItemRazzBerry,"http://i.imgur.com/9lyjJHn.png" } ,
-            {ItemId.ItemBlukBerry,"http://i.imgur.com/80WZSnw.png" } ,
-            {ItemId.ItemNanabBerry,"http://www.serebii.net/pokemongo/items/nanabberry.png" } ,
-            {ItemId.ItemPinapBerry,"http://www.serebii.net/pokemongo/items/pinapberry.png" } ,
-            {ItemId.ItemWeparBerry,"http://i.imgur.com/DOhPGVh.png" },
-            {ItemId.ItemIncenseOrdinary,"http://i.imgur.com/KqdjZTf.png" } ,
-            {ItemId.ItemTroyDisk,"http://i.imgur.com/qJedIld.png" } ,
-            {ItemId.ItemLuckyEgg,"http://i.imgur.com/fEPh6Eu.png" } ,
-            {ItemId.ItemDragonScale,"http://www.serebii.net/pokemongo/items/dragonscale.png" } ,
-            {ItemId.ItemUpGrade,"http://www.serebii.net/pokemongo/items/up-grade.png" },
-            {ItemId.ItemKingsRock, "http://www.serebii.net/pokemongo/items/kingsrock.png" },
-            { ItemId.ItemMetalCoat, "http://www.serebii.net/pokemongo/items/metalcoat.png" },
-            {ItemId.ItemSunStone, "http://www.serebii.net/pokemongo/items/sunstone.png" }
+            {ItemId.ItemIncubatorBasic,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/901.png" },
+            {ItemId.ItemIncubatorBasicUnlimited,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/902.png" },
+            {ItemId.ItemPokeBall,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/1.png" },
+            {ItemId.ItemGreatBall,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/2.png" },
+            {ItemId.ItemUltraBall,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/3.png" },
+            {ItemId.ItemMasterBall,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/4.png" },
+            {ItemId.ItemPotion,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/101.png" },
+            {ItemId.ItemSuperPotion,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/102.png" },
+            {ItemId.ItemHyperPotion,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/103.png" },
+            {ItemId.ItemMaxPotion,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/104.png" },
+            {ItemId.ItemRevive,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/201.png" },
+            {ItemId.ItemMaxRevive,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/202.png" },
+            {ItemId.ItemRazzBerry,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/701.png" },
+            {ItemId.ItemBlukBerry,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/702.png" },
+            {ItemId.ItemNanabBerry,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/703.png" },
+            {ItemId.ItemPinapBerry,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/705.png" },
+            {ItemId.ItemWeparBerry,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/704.png" },
+            {ItemId.ItemIncenseOrdinary,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/401.png" },
+            {ItemId.ItemTroyDisk,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/501.png" },
+            {ItemId.ItemLuckyEgg,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/301.png" },
+            {ItemId.ItemDragonScale,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/1104.png" },
+            {ItemId.ItemUpGrade,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/1105.png" },
+            {ItemId.ItemKingsRock,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/1102.png" },
+            {ItemId.ItemMetalCoat,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/1103.png" },
+            {ItemId.ItemSunStone,"https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/1101.png" }
         };
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -44,7 +44,7 @@ namespace PoGo.Necrobot.Window.Converters
             if (resources.ContainsKey(itemId))
                 return resources[itemId];
 
-            return "http://i.imgur.com/qJedIld.png";
+            return "https://raw.githubusercontent.com/Necrobot-Private/PokemonGO-Assets/master/items-icons/501.png";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
