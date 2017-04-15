9abae84dea40bf179059ff2d554c61685ad1b59f
Executive Dronpes <lordhajisinbox@gmail.com>
2017-04-15 19:49:29 +0530

    Fix Excel Exporting Bug
    Signed-off-by: Executive Dronpes <lordhajisinbox@gmail.com>

--- PoGo.NecroBot.Logic/DataDumper/Dumper.cs
+++ PoGo.NecroBot.Logic/DataDumper/Dumper.cs
@@ -126,12 +126,15 @@ namespace PoGo.NecroBot.Logic.DataDumper
                             ws.Cells[1, 9].Value = "HP";
                             ws.Cells[1, 10].Value = "MaxHP";
                             ws.Cells[1, 11].Value = "CP";
-                            ws.Cells[1, 12].Value = "Candy";
-                            ws.Cells[1, 13].Value = "Level";
+                            ws.Cells[1, 12].Value = "Level";
+                            ws.Cells[1, 13].Value = "Candy";
                             ws.Cells[1, 14].Value = "Move1";
                             ws.Cells[1, 15].Value = "Move2";
                             ws.Cells[1, 16].Value = "Type";
-                            ws.Cells[1, 16].Value = "Sex";
+                            ws.Cells[1, 17].Value = "Shiny";
+                            ws.Cells[1, 18].Value = "Form";
+                            ws.Cells[1, 19].Value = "Costume";
+                            ws.Cells[1, 20].Value = "Sex";
                             ws.Cells["A1:Q1"].AutoFilter = true;
                             ws.Cells["A1:Q1"].Style.Font.Bold = true;
                         }
@@ -140,19 +143,21 @@ namespace PoGo.NecroBot.Logic.DataDumper
                         ws.Cells[rowNum + 1, 3].Value = item.Nickname;
                         ws.Cells[rowNum + 1, 4].Value = item.OwnerName;
                         ws.Cells[rowNum + 1, 5].Value = Math.Round(PokemonInfo.CalculatePokemonPerfection(item), 2);
-
                         ws.Cells[rowNum + 1, 6].Value = item.IndividualAttack;
                         ws.Cells[rowNum + 1, 7].Value = item.IndividualDefense;
                         ws.Cells[rowNum + 1, 8].Value = item.IndividualStamina;
                         ws.Cells[rowNum + 1, 9].Value = item.Stamina;
                         ws.Cells[rowNum + 1, 10].Value = item.StaminaMax;
                         ws.Cells[rowNum + 1, 11].Value = PokemonInfo.CalculateCp(item);
-                        ws.Cells[rowNum + 1, 12].Value = session.Inventory.GetCandyCount(item.PokemonId);
-                        ws.Cells[rowNum + 1, 13].Value = PokemonInfo.GetLevel(item);
+                        ws.Cells[rowNum + 1, 12].Value = PokemonInfo.GetLevel(item);
+                        ws.Cells[rowNum + 1, 13].Value = session.Inventory.GetCandyCount(item.PokemonId);
                         ws.Cells[rowNum + 1, 14].Value = item.Move1.ToString();
                         ws.Cells[rowNum + 1, 15].Value = item.Move2.ToString();
                         ws.Cells[rowNum + 1, 16].Value = $"{settings.Type.ToString()},{settings.Type2.ToString()}";
-                        ws.Cells[rowNum + 1, 17].Value = $"{item.PokemonDisplay.Gender.ToString()}";
+                        ws.Cells[rowNum + 1, 17].Value = $"{item.Shiny ? "Yes" : "No"}";
+                        ws.Cells[rowNum + 1, 18].Value = $"{item.Form.ToString().Replace("Unown", "").Replace("Unset", "Normal")}";
+                        ws.Cells[rowNum + 1, 19].Value = $"{item.Costume.ToString().Replace("Unset", "Regular")}";
+                        ws.Cells[rowNum + 1, 20].Value = $"{item.PokemonDisplay.Gender.ToString().Replace("Less", "Genderless")}";
                         rowNum++;
                     }
                     package.Save();
