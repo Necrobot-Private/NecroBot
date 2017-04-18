bd3823149087a24213a6c2f4eef8e8c4001b2c8b
CDAGaming <videodude12>
2017-04-17 21:20:54 -0700

    Adds Images and Sets Default Theme

--- PoGo.Necrobot.Window/Properties/Settings.Designer.cs
+++ PoGo.Necrobot.Window/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace PoGo.Necrobot.Window.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Blue")]
         public string Theme {
             get {
                 return ((string)(this["Theme"]));
