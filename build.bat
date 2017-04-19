71fe7ed71b940bdcc28165a1f966912961b137bb
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2017-04-19 14:04:29 +0200

    Update nuget for vs2017

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-set necrobuilder=""
-for /f "delims=" %%i in ('dir /s /b /a-d "%programfiles(x86)%\MSBuild.exe"') do (if %necrobuilder%=="" (set necrobuilder="%%i"))
+for /f "delims=" %%i in ('dir /s /b /a-d "%programfiles(x86)%\MSBuild.exe"') do (set necrobuilder="%%i")
 %necrobuilder% "NecroBot-Private for Pokemon GO.sln" /property:Configuration="Release" /property:Platform="Any CPU"
 set necrobuilder=
