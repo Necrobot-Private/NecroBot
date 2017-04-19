9f62f1a53239fa6cbb721afcb894c55a765c9833
CDAGaming <videodude12>
2017-04-19 00:25:41 -0700

    Update Build.bat
    Updates Build.bat for Detecting VS2017 Installs and Building Accordingly

--- build.bat
+++ build.bat
@@ -1,23 +1,13 @@
 @echo off
-if exists "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE" goto VS2015_Community
-if exists "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community" goto VS2017_Community
+if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community" goto VS2017_Community
+if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise" goto VS2017_Enterprise
 
 :VS2017_Community
 echo Building under Visual Studio 2017 Community as %processor_architecture%
-if "%processor_architecture%"=="AMD64" (
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform=x64
-) ELSE (
-nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform=x86
-)
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform="Any CPU"
 
-:VS2015_Community
-echo Building under Visual Studio 2015 Community as %processor_architecture%
-if "%processor_architecture%"=="AMD64" (
+:VS2017_Enterprise
+echo Building under Visual Studio 2017 Enterprise as %processor_architecture%
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform=x64
-) ELSE (
-nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform=x86
-)
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform="Any CPU"
