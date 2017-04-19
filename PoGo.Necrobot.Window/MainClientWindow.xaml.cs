e31d7ebd1967f58238f16999bfd7b4bb8e1814e6
CDAGaming <videodude12>
2017-04-18 17:53:39 -0700

    Fix for Dark Mode Reverting Theme, and vice versa

--- PoGo.Necrobot.Window/MainClientWindow.xaml.cs
+++ PoGo.Necrobot.Window/MainClientWindow.xaml.cs
@@ -105,7 +105,7 @@ namespace PoGo.Necrobot.Window
             Height = Settings.Default.Height;
             if (datacontext.PlayerInfo.Level == 35) // Warn Player on Reaching this Level -- NEEDS CONFIG SETTING
             {
-                NecroBot.Logic.Logging.Logger.Write($"You have reached Level {datacontext.PlayerInfo.Level} and it is recommended to Switch Accounts",LogLevel.Warning);
+                Logger.Write($"You have reached Level {datacontext.PlayerInfo.Level} and it is recommended to Switch Accounts",LogLevel.Warning);
             }
             ChangeThemeTo(Settings.Default.Theme);
             ChangeSchemeTo(Settings.Default.Scheme);
@@ -211,7 +211,21 @@ namespace PoGo.Necrobot.Window
             Application.Current.Resources.MergedDictionaries.Add(dict);
             Application.Current.Resources.MergedDictionaries.Remove(theme);
 
+            if (Settings.Default.Scheme != "BaseLight") // If Not Equivalent to Default
+            {
+                ChangeSchemeTo_KeepTheme(Settings.Default.Scheme);
+            }
         }
+
+        private void ChangeThemeTo_KeepScheme(string Theme)
+        {
+            ResourceDictionary dict = new ResourceDictionary()
+            {
+                Source = new Uri($"pack://application:,,,/MahApps.Metro;component/Styles/Accents/{Theme}.xaml", UriKind.Absolute)
+            };
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
         private void ChangeSchemeTo(string Scheme)
         {
             ResourceDictionary dict = new ResourceDictionary()
@@ -222,28 +236,19 @@ namespace PoGo.Necrobot.Window
             Application.Current.Resources.MergedDictionaries.Add(dict);
             Application.Current.Resources.MergedDictionaries.Remove(scheme);
 
-            /*if (Settings.Default.Scheme == "BaseLight")
+            if (Settings.Default.Theme != "Blue") // If not Equivalent to Default
             {
-                tabAccounts.Background = Brushes.Gray;
-                tabBrowser.Background = Brushes.Gray;
-                tabConsole.Background = Brushes.Gray;
-                tabEggs.Background = Brushes.Gray;
-                tabItems.Background = Brushes.Gray;
-                tabPokemons.Background = Brushes.Gray;
-                tabMap.Background = Brushes.Gray;
-                tabSniper.Background = Brushes.Gray;
+                ChangeThemeTo_KeepScheme(Settings.Default.Theme);
             }
-            else if (Settings.Default.Scheme == "BaseDark")
+        }
+
+        private void ChangeSchemeTo_KeepTheme(string Scheme)
+        {
+            ResourceDictionary dict = new ResourceDictionary()
             {
-                tabAccounts.Background = Brushes.Black;
-                tabBrowser.Background = Brushes.Black;
-                tabConsole.Background = Brushes.Black;
-                tabEggs.Background = Brushes.Black;
-                tabItems.Background = Brushes.Black;
-                tabPokemons.Background = Brushes.Black;
-                tabMap.Background = Brushes.Black;
-                tabSniper.Background = Brushes.Black;
-            }*/
+                Source = new Uri($"pack://application:,,,/MahApps.Metro;component/Styles/Accents/{Scheme}.xaml", UriKind.Absolute)
+            };
+            Application.Current.Resources.MergedDictionaries.Add(dict);
         }
 
         private void Theme_Selected(object sender, RoutedEventArgs e)
