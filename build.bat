9a453e8edfb3fd2fff2fe9bdc1d496f986160384
CDAGaming <videodude12>
2017-04-19 09:09:45 -0700

    Updates Build.bat to Match Futrif's
    Matches https://github.com/Necrobot-Private/NecroBot/pull/1538

--- build.bat
+++ build.bat
@@ -1,13 +1,5 @@
 @echo off
-if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community" goto VS2017_Community
-if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise" goto VS2017_Enterprise
-
-:VS2017_Community
-echo Building under Visual Studio 2017 Community as %processor_architecture%
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform="Any CPU"
-
-:VS2017_Enterprise
-echo Building under Visual Studio 2017 Enterprise as %processor_architecture%
-nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" "NecroBot-Private for Pokemon GO.sln" /property:Configuration=Release /property:Platform="Any CPU"
+for /f "delims=" %%i in ('dir /s /b /a-d "%programfiles(x86)%\MSBuild.exe"') do (set necrobuilder="%%i")
+%necrobuilder% "NecroBot-Private for Pokemon GO.sln" /property:Configuration="Release" /property:Platform="Any CPU"
+set necrobuilder=
