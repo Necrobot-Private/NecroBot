c026d9458677bccbdf3c7d5abf656d72aaeda17b
appveyor <appveyor@necrobot.com>
2017-05-02 06:41:43 -0700

    v1.0.0.163

--- PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion1("1.0.0.134")]
 
 
-[assembly: AssemblyVersion("1.0.0.162")]
+[assembly: AssemblyVersion("1.0.0.163")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v1.0.0.162")]
+[assembly: AssemblyInformationalVersion("v1.0.0.163")]
