f7630e8d0a490982c8a74ae6e6977a03c294926f
jjskuld <jjskuld@gmail.com>
2017-05-03 10:45:56 -0700

    Add initial database generated from SQLite

---
+++ PoGo.NecroBot.Logic/Model/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PoGo.NecroBot.Logic.Model
+{
+    public partial class Account
+    {
+        public long Id { get; set; }
+        public long AuthType { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public double? RuntimeTotal { get; set; }
+        public long? LastRuntimeUpdatedAt { get; set; }
+        public long? ReleaseBlockTime { get; set; }
+        public string Nickname { get; set; }
+        public long? LoggedTime { get; set; }
+        public long? Level { get; set; }
+        public string LastLogin { get; set; }
+        public long? LastLoginTimestamp { get; set; }
+        public long? Stardust { get; set; }
+        public long? CurrentXp { get; set; }
+        public long? PrevLevelXp { get; set; }
+        public long? NextLevelXp { get; set; }
+    }
+}
---
+++ PoGo.NecroBot.Logic/Model/DatabaseConfigContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PoGo.NecroBot.Logic.Model
+{
+    public partial class DatabaseConfigContext : DbContext
+    {
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlite(@"data source=C:\Users\Joe\Dev\NecroBot\PoGo.NecroBot.Logic\Resources\config.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
