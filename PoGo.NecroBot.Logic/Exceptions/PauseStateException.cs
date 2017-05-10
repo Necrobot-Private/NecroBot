bbd06e03b0414dc2dbdfb6b754d3378db5d4efdd
CDAGaming <cstack2011@yahoo.com>
2017-05-09 21:03:54 -0500

    Revert and Try Again, but Adds PauseState and Some Functions

---
+++ PoGo.NecroBot.Logic/Exceptions/PauseStateException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoGo.NecroBot.Logic.Exceptions
+{
+    public class PauseStateException : Exception
+    {
+        public bool IsRunning;
+
+        public PauseStateException(bool isRunning)
+        {
+            IsRunning = isRunning;
+        }
+    }
+}
