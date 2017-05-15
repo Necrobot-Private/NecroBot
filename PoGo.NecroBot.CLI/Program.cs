020baebfbaa7cf6a5e430afead67dc1a7a42348a
CDAGaming <cstack2011@yahoo.com>
2017-05-14 21:47:42 -0500

    Adjust Program.CS Commands

--- PoGo.NecroBot.CLI/Program.cs
+++ PoGo.NecroBot.CLI/Program.cs
@@ -33,22 +33,20 @@ namespace PoGo.NecroBot.CLI
 {
     class Options
     {
-        [Option('i', "init", Required = false,
-          HelpText = "Init account")]
+        [Option('i', "init", Required = false, HelpText = "Init account")]
         public bool Init { get; set; }
 
         [Option('t', "template", DefaultValue = "", Required = false , HelpText = "Prints all messages to standard output.")]
         public string Template { get; set; }
 
-        [Option('p', "password", DefaultValue = "", Required = false, HelpText = "pasword")]
+        [Option('p', "password", DefaultValue = "", Required = false, HelpText = "password")]
         public string Password { get; set; }
 
-        [Option('g', "google", DefaultValue = false, Required = false,HelpText = "is google account")]
+        [Option('g', "google", DefaultValue = false, Required = false, HelpText = "is google account")]
         public bool IsGoogle{ get; set; }
 
-        [Option('s', "start", DefaultValue = 1,HelpText = "Start account", Required = false)]
+        [Option('s', "start", DefaultValue = 1, HelpText = "Start account", Required = false)]
         public int Start { get; set; }
-
 
         [Option('e', "end", DefaultValue = 10, HelpText = "End account",Required = false)]
         public int End { get; set; }
