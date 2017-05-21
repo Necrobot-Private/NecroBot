401cc441646dbbede67170a9ae58005500231798
CDAGaming <cstack2011@yahoo.com>
2017-05-20 23:57:50 -0500

    Attempted Fix for 0KB Update Downloads, and OverHauls download link to new format
    Signed-off-by: CDAGaming <cstack2011@yahoo.com>

--- PoGo.NecroBot.Logic/State/VersionCheckState.cs
+++ PoGo.NecroBot.Logic/State/VersionCheckState.cs
@@ -30,7 +30,7 @@ namespace PoGo.NecroBot.Logic.State
             "https://api.github.com/repos/Necrobot-Private/NecroBot/releases/latest";
 
         public const string RemoteReleaseUrl =
-            "https://bintray.com/necrobot-private/NecroBot2/download_file?file_path=";
+            "https://github.com/Necrobot-Private/NecroBot/releases/download/v";
 
         public static Version RemoteVersion;
 
@@ -64,16 +64,13 @@ namespace PoGo.NecroBot.Logic.State
             }
 
             SystemSounds.Asterisk.Play();
-            
-            var remoteReleaseUrl =
-                $"https://github.com/Necrobot-Private/NecroBot/releases/download/v{RemoteVersion}/";
-            
+
             string zipName = $"NecroBot2.Console.{RemoteVersion.ToString()}.zip";
             if (Assembly.GetEntryAssembly().FullName.ToLower().Contains("necrobot2.win"))
             {
                 zipName = $"NecroBot2.WIN.{RemoteVersion.ToString()}.zip";
             }
-            var downloadLink = remoteReleaseUrl + zipName;
+            var downloadLink = $"{RemoteReleaseUrl}{RemoteVersion}/{zipName}";
 
             var baseDir = Directory.GetCurrentDirectory();
             var downloadFilePath = Path.Combine(baseDir, zipName);
