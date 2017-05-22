46eee7be3dace3a770ba5b600c9b006d8ec9f424
Executive Dronpes <lordhajisinbox@gmail.com>
2017-05-22 23:46:53 +0530

    WIP PokeCoin Collection feature

--- PoGo.Necrobot.Window/Controls/PlayerInfo.xaml.cs
+++ PoGo.Necrobot.Window/Controls/PlayerInfo.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using PoGo.Necrobot.Window.Model;
+using PoGo.NecroBot.Logic.State;
 using System;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace PoGo.Necrobot.Window.Controls
 {
@@ -12,6 +12,7 @@ namespace PoGo.Necrobot.Window.Controls
 
     public partial class PlayerInfo : UserControl
     {
+        public ISession Session { get; set; }
 
         public String Label
         {
@@ -40,6 +41,11 @@ namespace PoGo.Necrobot.Window.Controls
             //this.DataContext = this.PlayerData;
             InitializeComponent();
         }
-                           
+
+        private async void GetPokeCoin_Click(object sender, RoutedEventArgs e)
+        {
+            await Session.Client.Player.CollectDailyBonus();
+        }
+
     }
 }
