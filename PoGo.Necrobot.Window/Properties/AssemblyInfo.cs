324aa515bd034032a33a22a335fd9ff141e9590c
appveyor <appveyor@necrobot.com>
2017-05-23 10:29:05 -0700

    v1.0.0.176

--- PoGo.Necrobot.Window/Properties/AssemblyInfo.cs
+++ PoGo.Necrobot.Window/Properties/AssemblyInfo.cs
@@ -48,9 +48,9 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.175")]
+// [assembly: AssemblyVersion("1.0.0.176")]
 
 
-[assembly: AssemblyVersion("1.0.0.175")]
+[assembly: AssemblyVersion("1.0.0.176")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v1.0.0.175")]
+[assembly: AssemblyInformationalVersion("v1.0.0.176")]
