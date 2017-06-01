acb8b61c0c554d3f2f5ce4dbdc7932039ecf6bf0
CDAGaming <cstack2011@yahoo.com>
2017-05-31 23:26:02 -0700

    Build and KillSwitch Adjustments
    Signed-off-by: CDAGaming <cstack2011@yahoo.com>

--- build.bat
+++ build.bat
@@ -1,27 +1,27 @@
 @echo off
-echo Build preparation....
+echo Build Preparation....
 cd %CD%
-echo Select you platform (10 secs - def = Any CPU)
+echo Select your Platform: (10 Seconds = Any CPU)
 echo.
 echo 1 = Any CPU 
 echo 2 = x86
 echo 3 = x64
-CHOICE /C 123 /N /T 10 /D 1 /M "Select you build:"
+CHOICE /C 123 /N /T 10 /D 1 /M "Select your Build:"
 IF ERRORLEVEL 1 SET Platform=Any CPU
 IF ERRORLEVEL 2 SET Platform=x86
 IF ERRORLEVEL 3 SET Platform=x64
-ECHO You chose %Platform%
+ECHO %Platform% Selected as Build Type!
 echo.
-echo Select you configuration (10 secs - def = Release)
+echo Select your Configuration: (10 Seconds = Release)
 echo.
 echo 1 = Release
 echo 2 = Debug
-CHOICE /C 12 /N /T 10 /D 1 /M "Select you build configuration:"
+CHOICE /C 12 /N /T 10 /D 1 /M "Select your Build Configuration:"
 IF ERRORLEVEL 1 SET Release=Release
 IF ERRORLEVEL 2 SET Release=Debug
-ECHO You chose %Release%
+echo %Release% Selected as Build Configuration!
 echo.
-echo Build initialised %Release% - %Platform%
+echo Build Starting... (%Release% - %Platform%)
 echo.
 nuget.exe restore "NecroBot-Private for Pokemon GO.sln"
 for /f "delims=" %%i in ('dir /s /b /a-d "%programfiles(x86)%\MSBuild.exe"') do (set necrobuilder="%%i")
