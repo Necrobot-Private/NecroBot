31277e136ba698680cc900e084cf20e378143395
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-06-05 19:08:05 +0200

    fix multiaccount methodes

--- PoGo.NecroBot.Logic/MultiAccountManager.cs
+++ PoGo.NecroBot.Logic/MultiAccountManager.cs
@@ -364,16 +364,18 @@ namespace PoGo.NecroBot.Logic
         {
             foreach (var item in Accounts)
             {
-                var L = item.Nickname.Length;
                 var SP = "";
-                for (int i = 0; i < 31 - L; i++)
-                {
-                    SP += " ";
-                }
+                var user = !string.IsNullOrEmpty(item.Nickname) ? item.Nickname : item.Username;
+
+                var L = user.Length;
+                    for (int i = 0; i < 31 - L; i++)
+                    {
+                        SP += " ";
+                    }
                 if (item.Level > 0)
-                    Logging.Logger.Write($"{item.Nickname}{SP}(Level: {item.Level:#0}) | Runtime: {item.RuntimeTotal:00:00:00}");
+                    Logging.Logger.Write($"{user}{SP}(Level: {item.Level:#0}) | Runtime: {item.RuntimeTotal:00:00:00}");
                 else
-                    Logging.Logger.Write($"{item.Nickname}{SP}(Level: ??) | Runtime: {item.RuntimeTotal:00:00:00}");
+                    Logging.Logger.Write($"{user}{SP}(Level: ??) | Runtime: {item.RuntimeTotal:00:00:00}");
             }
         }
 
