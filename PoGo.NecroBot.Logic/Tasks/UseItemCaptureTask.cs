bcecd0c1219026e2d7aae321868b094d5642b94f
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-07-20 23:19:10 +0200

    Update to 0.69.0 needed PR https://github.com/Necrobot-Private/PokemonGo.RocketAPI/pull/147 Add UseItemCapture Added UseItemCaptureTask

---
+++ PoGo.NecroBot.Logic/Tasks/UseItemCaptureTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using PoGo.NecroBot.Logic.Logging;
+using PoGo.NecroBot.Logic.State;
+using POGOProtos.Inventory.Item;
+using POGOProtos.Networking.Responses;
+using POGOProtos.Map.Fort;
+
+namespace PoGo.NecroBot.Logic.Tasks
+{
+    public class UseItemCaptureTask
+    {
+        public static async Task Execute(ISession session, ItemId itemid, ulong encounterid, FortData fort)
+        {
+            string UsedTo = encounterid > 0 ? "Pokemon" : "Fort";
+            Logger.Write($"Use {itemid.ToString()} as {UsedTo}", LogLevel.Recycling);
+            await session.Client.Inventory.UseItemCapture(itemid, encounterid, fort.Id).ConfigureAwait(false);
+        }
+    }
+}
