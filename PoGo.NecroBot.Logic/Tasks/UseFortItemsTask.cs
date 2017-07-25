bba3bfb9cd540997b4a57bf0075b012575eb2f76
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-07-25 13:48:34 +0200

    Added UseFortitemsTask for use lure add use lure into rocketbot other fixes

---
+++ PoGo.NecroBot.Logic/Tasks/UseFortItemsTask.cs
@@ -0,0 +1,45 @@
+﻿using PoGo.NecroBot.Logic.Logging;
+using PoGo.NecroBot.Logic.State;
+using POGOProtos.Inventory.Item;
+using POGOProtos.Map.Fort;
+using POGOProtos.Networking.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Logic.Tasks
+{
+    public class UseFortItemsTask
+    {
+        public static async Task Execute(ISession session, FortData fort, ItemData item)
+        {
+            var response = await session.Client.Fort.AddFortModifier(fort.Id, item.ItemId).ConfigureAwait(false);
+            switch (response.Result)
+            {
+                case AddFortModifierResponse.Types.Result.Success:
+                    Logger.Write($"{item.ItemId} is valid until: {DateTime.Now.AddMinutes(30)}");
+                    break;
+                case AddFortModifierResponse.Types.Result.FortAlreadyHasModifier:
+                    Logger.Write($"An {item.ItemId} is already active!", LogLevel.Warning);
+                    break;
+                case AddFortModifierResponse.Types.Result.NoItemInInventory:
+                    Logger.Write($"{item.ItemId} no found!", LogLevel.Error);
+                    break;
+                case AddFortModifierResponse.Types.Result.NoResultSet:
+                    Logger.Write($"{item.ItemId} no result set!", LogLevel.Error);
+                    break;
+                case AddFortModifierResponse.Types.Result.PoiInaccessible:
+                    Logger.Write($"{item.ItemId} poi inaccessible!", LogLevel.Error);
+                    break;
+                case AddFortModifierResponse.Types.Result.TooFarAway:
+                    Logger.Write($"{item.ItemId} too far away!", LogLevel.Error);
+                    break;
+                default:
+                    Logger.Write($"Failed to use an {item.ItemId}!", LogLevel.Error);
+                    break;
+            }
+        }
+    }
+}
